69e35e20cf2f6665d3fe89d8f8e0d74ec1ff1431
Ben Milton <ben.c.milton@gmail.com>
2015-04-18 11:45:13 -0400

    added password hashing

--- app/models/user.js
+++ app/models/user.js
@@ -1,19 +1,48 @@
 var mongoose = require('mongoose');
 var deepPopulate = require('mongoose-deep-populate');
+var crypto = require('crypto');
 
 var User;
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
     email: {type: String, required: true},
-    password: {type: String, required: true},
-    moviesLists: [{type: Schema.Types.ObjectId, ref: 'MovieList'}]
-    //moviesLists: [Schema.Types.MovieList]
+    hashedPassword: {type: String, required: true},
+    moviesLists: [{type: Schema.Types.ObjectId, ref: 'MovieList'}],
+    salt: {
+        type: String,
+        default: makeSalt
+    },
+    facebook: {
+        id: String,
+        token: String,
+        name: String,
+        email: String
+    }
 });
 
+//generate salt key
+function makeSalt () {
+    return crypto.randomBytes(16).toString('base64');
+}
+
+//virtual method for automatically setting hashed user passwords
+UserSchema.virtual('password')
+    .set(function (plaintext) {
+        this.hashedPassword = this.hash(plaintext);
+    });
+
+//user method that hashes a plain text password
+UserSchema.methods.hash = function (plaintext) {
+    return crypto.pbkdf2Sync(plaintext, this.salt, 10000, 64).toString('base64');
+};
+
+//method for comparing user login attempt to stored hashed password
+UserSchema.methods.authenticate = function (attempt) {
+    return this.hash(attempt) == this.hashedPassword;
+};
+
+//initialize deep populate plugin
 UserSchema.plugin(deepPopulate,{});
 
-User = mongoose.model('User', UserSchema);
-
-
-module.exports = User;
+module.exports = mongoose.model('User', UserSchema);
